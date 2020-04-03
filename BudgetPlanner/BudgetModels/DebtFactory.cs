c0da34ecaa78685884b2f0fa705c5728adcb531a
Daxxn <46167982+Daxxn@users.noreply.github.com>
2020-04-03 16:26:21 -0700

    Started adding classes
    Need to integrate from another branch first

---
+++ BudgetPlanner/BudgetModels/DebtFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetModels
+{
+    public static class DebtFactory
+	{
+		#region - Fields & Properties
+
+		#endregion
+
+		#region - Methods
+
+		#endregion
+
+		#region - Full Properties
+
+		#endregion
+	}
+}
